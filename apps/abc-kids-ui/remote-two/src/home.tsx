2952ee441c6e003fefbd66997a7367ca5b16a2b9
Sean Falese <falese@gmail.com>
2024-04-06 22:57:57 -0400

    updated image and items

--- apps/abc-kids-ui/remote-two/src/home.tsx
+++ apps/abc-kids-ui/remote-two/src/home.tsx
@@ -1,8 +1,10 @@
+import image from "./Untitled.png";
+
 export default function Home() {
   const items = [
-    { name: "game1", description: "the first game for abc kids" },
-    { name: "game2", description: "the second game for abc kids" },
-    { name: "game3", description: "the third game for abc kids" },
+    { name: "Game Number One", description: "the first game for abc kids" },
+    { name: "Game Number Two", description: "the second game for abc kids" },
+    { name: "Game Number Three", description: "the third game for abc kids" },
   ];
 
   return (
@@ -13,10 +15,7 @@ export default function Home() {
             <h2> {item.name} </h2>
             <span>{item.description}</span>
           </div>
-          <img
-            src="https://s3.ap-south-1.amazonaws.com/s3.studytonight.com/tutorials/uploads/pictures/1627731351-101156.png"
-            alt="img"
-          />
+          <img src={image} />
         </div>
       ))}
     </div>
