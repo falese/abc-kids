c0abbfb256eaa975ec2766efa105073b89f47b35
Sean Falese <falese@gmail.com>
2024-04-07 18:51:07 -0400

    build

--- apps/abc-kids-ui/remote-two/src/home.tsx
+++ apps/abc-kids-ui/remote-two/src/home.tsx
@@ -1,10 +1,29 @@
-import image from "./Untitled.png";
+import foxes from './images/foxes.png'
+import hockey from './images/hockey.png'
+import untitled from './images/Untitled.png'
+
 
 export default function Home() {
+ const foxesImage = foxes
+ const hockeyImage = hockey
+ const untitledImage = untitled
+ 
   const items = [
-    { name: "Game Number One", description: "the first game for abc kids" },
-    { name: "Game Number Two", description: "the second game for abc kids" },
-    { name: "Game Number Three", description: "the third game for abc kids" },
+    {
+      name: "Game Number One",
+      description: "the first game for abc kids",
+      image: foxesImage,
+    },
+    {
+      name: "Game Number Two",
+      description: "the second game for abc kids",
+      image: hockeyImage,
+    },
+    {
+      name: "Game Number Three",
+      description: "the third game for abc kids",
+      image: untitledImage,
+    },
   ];
 
   return (
@@ -15,7 +34,7 @@ export default function Home() {
             <h2> {item.name} </h2>
             <span>{item.description}</span>
           </div>
-          <img src={image} />
+          <img src={item.image} />
         </div>
       ))}
     </div>
