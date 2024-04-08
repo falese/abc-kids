30b21e999f07ebab5b92426ae1d55726070e06b7
Sean Falese <falese@gmail.com>
2024-04-07 21:21:40 -0400

    refactor static

--- apps/abc-kids-ui/publish/remote-two/dist/@mf-types.d.ts
+++
@@ -1,3 +0,0 @@
-
-    export type RemoteKeys = 'REMOTE_ALIAS_IDENTIFIER/home';
-    type PackageType<T> = T extends 'REMOTE_ALIAS_IDENTIFIER/home' ? typeof import('REMOTE_ALIAS_IDENTIFIER/home') :any;
